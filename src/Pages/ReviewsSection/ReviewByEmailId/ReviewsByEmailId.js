2ab3870ca28cd65fa9aa44624487d32fce3cd726
suhanasalma <suhanasalma800@gmail.com>
2022-11-09 05:43:54 +0600

    delete comments succesfully

--- src/Pages/ReviewsSection/ReviewByEmailId/ReviewsByEmailId.js
+++ src/Pages/ReviewsSection/ReviewByEmailId/ReviewsByEmailId.js
@@ -1,23 +1,63 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../../SharedContext/SharedContext';
+import ReviewCard from '../ReviewCard/ReviewCard';
 
 
 const ReviewsByEmailId = () => {
-   const [allReviews, setAllReviewa] = useState([])
+   const [allReviews, setAllReview] = useState([])
    const {user} = useContext(AuthContext)
 
    useEffect(()=>{
       fetch(`http://localhost:5000/comments?email=${user?.email}`)
       .then(res=>res.json())
       .then(data=>{
-         console.log(data)
+         // console.log(data)
+         setAllReview(data)
       })
-   },[])
+   },[user?.email])
+
+   const handleDelete = (id) =>{
+      console.log(id)
+      const proceed = window.confirm('do you want to delete it?')
+      if(proceed){
+         fetch(`http://localhost:5000/comments/${id}`, {
+         method: "DELETE",
+         })
+         .then(res=>res.json())
+         .then(data=>{
+            console.log(data)
+            const remaingReview = allReviews.filter((item) => item._id !== id);
+            setAllReview(remaingReview)
+           })
+      }
+      
+
+   }
+
+
 
    return (
-      <div>
-         
-      </div>
+     <div>
+       <div className="overflow-x-auto w-full my-20">
+         <table className="table w-full">
+           <thead>
+             <tr>
+               <th>Delete</th>
+               <th>Service</th>
+               <th>Name</th>
+               <th>Review</th>
+               <th>Time</th>
+               <th>Edit</th>
+             </tr>
+           </thead>
+           <tbody>
+             {allReviews.map((item) => (
+               <ReviewCard item={item} handleDelete={handleDelete} key={item._id}></ReviewCard>
+             ))}
+           </tbody>
+         </table>
+       </div>
+     </div>
    );
 };
 
---
+++ src/Pages/ReviewsSection/ReviewCard/ReviewCard.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+
+const ReviewCard = ({ item, handleDelete }) => {
+  const { name, image, review, time, post,_id } = item;
+
+  // console.log(handleDelete);
+  const [service, setService] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/services/${post}`)
+      .then((res) => res.json())
+      .then((data) => setService(data));
+  }, []);
+
+  return (
+    <tr>
+      <th>
+        <button onClick={()=>handleDelete(_id)} className='text-red-700'>X</button>
+      </th>
+      <td>
+        <div className="flex items-center space-x-3">
+          <div className="avatar">
+            <div className="mask mask-squircle w-12 h-12">
+              <img src={service.img} alt="Avatar Tailwind CSS Component" />
+            </div>
+          </div>
+          <div>
+            <div className="font-bold">{service.name}</div>
+          </div>
+        </div>
+      </td>
+      <td>
+        <div className="flex items-center space-x-3">
+          <div className="avatar">
+            <div className="mask mask-squircle w-12 h-12">
+              <img src={image} alt="Avatar Tailwind CSS Component" />
+            </div>
+          </div>
+          <div>
+            <div className="font-bold">{name}</div>
+            <div className="text-sm opacity-50">Dhaka</div>
+          </div>
+        </div>
+      </td>
+      <td>{review}</td>
+      <td>{time}</td>
+      <th>
+        <button className="btn btn-ghost btn-xs">Edit</button>
+      </th>
+    </tr>
+  );
+};
+
+export default ReviewCard;
